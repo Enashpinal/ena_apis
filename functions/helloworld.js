9f9800f320b50bf500b00d8aca20eda8766ebd2e
Enashpinal <409894128@qq.com>
2024-10-20 23:32:02 +0800

    Update helloworld.js

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,3 +1,5 @@
-export function onRequest(context) {
-  return new Response("Hello, world!")
+export async function onRequest(context) {
+    return new Response(JSON.stringify({ message: "Hello, World!" }), {
+        headers: { "Content-Type": "application/json" },
+    });
 }
